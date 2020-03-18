9d811c8953fa9779db834865b28fa313256f9780
Jingyu Bao <zhongjzsb@gmail.com>
2020-03-18 17:40:31 -0400

    update datatable

--- R/07-datatable.R
+++ R/07-datatable.R
@@ -1,19 +1,40 @@
 library(DT)
+library(countrycode)
 
-wide_data <- dcast(data, ...~Type, value.var = 'Num')
-latest_data <- wide_data[Date==max(Date) & confirmed!=0]
+wide_data <- dcast(
+    country_data[Date==max(Date),], ...~Type, value.var = 'Num'
+)[order(-current)]
+latest_data <- dcast(
+    country_data[Date==max(Date),], ...~Type, value.var = 'Num'
+)[, `:=`(
+    Ctry=countrycode(
+        `Country/Region`, 'country.name', 'iso3c', warn=FALSE, nomatch = NULL
+    ),
+    Cur=current,
+    D=death,
+    Rec=recovered
+)][, .(
+    Ctry,
+    Cur,
+    D,
+    Rec
+)][order(-Cur)]
 
-latest_data_DT <- latest_data[, .(
-    `Country/Region`,
-    `Province/State`,
-    Date,
-    confirmed,
-    current,
-    death,
-    recovered
-)][order(-current)]
+latest_data_DT_mobile <- datatable(
+    latest_data,
+    width = 250,
+    height = 400,
+    rownames = FALSE,
+    options = list(pageLength=5)
+)
 
-latest_data_DT <- datatable(latest_data_DT)
+latest_data_DT <- datatable(
+    wide_data,
+    width = 800,
+    height = 400,
+    rownames = FALSE,
+    options = list(pageLength=10)
+)
 
 htmlwidgets::saveWidget(
     latest_data_DT,
@@ -23,3 +44,20 @@ htmlwidgets::saveWidget(
         'covid-19-DT.html'),
     selfcontained = FALSE,
     libdir = 'r_js')
+
+htmlwidgets::saveWidget(
+    latest_data_DT_mobile,
+    here::here(
+        "static",
+        "images",
+        'covid-19-DT-mobile.html'),
+    selfcontained = FALSE,
+    libdir = 'r_js')
+
+# writeLines(format_table(latest_data[1:20], format = 'html'),
+#            here::here(
+#                "static",
+#                "images",
+#                'covid-19-DT.txt'))
+
+
